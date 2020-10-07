a8cdaba28f026dc1619cb3b364a59e1b05bc20bb
mithrax <rahul.anthonym@gmail.com>
2020-10-06 22:02:12 -0400

    set initial states for user auth fields

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+
   return (
     <div className="App">
       <h1>Hello let's do this</h1>
