1d2bd8821a27bbfab4da24aa87118eaa9af0bb5c
mithrax <rahul.anthonym@gmail.com>
2020-10-07 22:33:40 -0400

    set props to pass to login component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import fire from "./fire";
 import classes from "./App.module.css";
 import { Jumbotron, Form, Button } from "react-bootstrap/";
 
-// import Login from "../src/components/Login/Login";
+import Login from "../src/components/Login/Login";
 // import Signup from "../src/components/Signup/Signup";
 
 function App() {
@@ -85,6 +85,18 @@ function App() {
     <div className={classes.App}>
       <Jumbotron>
         <h1 className={classes.AppHeader}> GigEco Code Challenge</h1>
+        <Login
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          loginHandler={loginHandler}
+          signupHandler={signupHandler}
+          hasAccount={hasAccount}
+          setHasAccount={setHasAccount}
+          emailError={emailError}
+          passwordError={passwordError}
+        />
       </Jumbotron>
     </div>
   );
