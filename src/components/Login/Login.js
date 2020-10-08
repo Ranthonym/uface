ebe7a777cd369524bb6c8127e735c6f406e7f17d
mithrax <rahul.anthonym@gmail.com>
2020-10-07 22:52:10 -0400

    re-create login/signup form to follow firebase handler functions passed down as props

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,19 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import classes from "./Login.module.css";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Login = (props) => {
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loginHandler,
+    signupHandle,
+    hasAccount,
+    setHasAccount,
+    emailError,
+    passwordError,
+  } = props;
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -16,34 +26,42 @@ const Login = () => {
 
   return (
     <div className={classes.Login}>
-      <h1>User Login</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            data-ms-member="email"
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter email"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            value={password}
-            data-ms-member="password"
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Button variant="primary" disabled={!validateForm()} type="submit">
-          Login
-        </Button>
-      </Form>
+      <div className={classes.LoginContainer}>
+        <label>Username</label>
+        <input
+          type="text"
+          autoFocus
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p className={classes.ErrorMsg}>{emailError}</p>
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p className={classes.ErrorMsg}>{passwordError}</p>
+        <div className={classes.BtnContainer}>
+          {hasAccount ? (
+            <>
+              <button onClick={loginHandler}>Sign in</button>
+              <p>
+                Need an acount? <span>Sign up</span>
+              </p>
+            </>
+          ) : (
+            <>
+              <button>Sign up</button>
+              <p>
+                Already have an account? <span>Login</span>
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
