36993eb1793af5a5f03f54067a83d0515ab9efcf
mithrax <rahul.anthonym@gmail.com>
2020-10-07 22:24:34 -0400

    create login, signup and logout handler functions with firebase auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
+import fire from "./fire";
 
 import classes from "./App.module.css";
-
 import { Jumbotron, Form, Button } from "react-bootstrap/";
 
-import Login from "../src/components/Login/Login";
-import Signup from "../src/components/Signup/Signup";
+// import Login from "../src/components/Login/Login";
+// import Signup from "../src/components/Signup/Signup";
 
 function App() {
   const [user, setUser] = useState("");
@@ -13,18 +13,61 @@ function App() {
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
-  const [userAuth, setUserAuth] = useState(false);
+  const [hasAccount, setHasAccount] = useState(false);
 
-  const loginHandler = () => {};
+  const loginHandler = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
 
-  const registrationHandler = () => {};
+  const signupHandler = () => {
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  };
+
+  const logoutHandler = () => {
+    fire.auth().signOut();
+  };
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
 
   return (
     <div className={classes.App}>
       <Jumbotron>
         <h1 className={classes.AppHeader}> GigEco Code Challenge</h1>
-        <Login />
-        <Signup />
       </Jumbotron>
     </div>
   );
